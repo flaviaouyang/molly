4e45405b1a95f8052b3bf478a6ec9d329b3c7140
Flavia Ouyang <flavia.ouyang@mail.mcgill.ca>
2024-03-03 17:54:33 -0500

    Implement parse date method

--- molly/utility/datetime.py
+++ molly/utility/datetime.py
@@ -1,5 +1,36 @@
-from datetime import datetime
+import logging
+from datetime import datetime, timedelta, timezone
+from typing import Tuple, Union
+
+logger = logging.getLogger(__name__)
 
 
-def parse_date():
-    pass
+def parse_date(value: Union[str, int, datetime, Tuple], **kwargs) -> datetime:
+    if isinstance(value, datetime):
+        logger.debug(f"Value {value} is already a datetime object. Returning as is.")
+        return value
+
+    utc_now = datetime.now(tz=timezone.utc)
+    if isinstance(value, int):
+        value = ("days", value)
+    elif isinstance(value, str):
+        parsed_value = datetime.strptime(value, **kwargs)
+
+    if isinstance(value, Tuple):
+        unit, numeric_value = value
+        assert unit in [
+            "days",
+            "seconds",
+            "microseconds",
+            "milliseconds",
+            "minutes",
+            "hours",
+            "weeks",
+        ], f"Unsupported unit: {unit}. Supported units are days, seconds, microseconds, milliseconds, minutes, hours, weeks."
+        assert isinstance(
+            numeric_value, int
+        ), f"If value is a tuple, it should be (str, int) but got (str, {type(numeric_value)})"
+        parsed_value = utc_now + timedelta(**{unit: numeric_value})
+
+    logger.debug(f"Parsed {value} to {parsed_value}")
+    return parsed_value
