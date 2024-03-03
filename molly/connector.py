a6d58490c0e100a3b1f32b0f1a8ffc8dd1726fec
Flavia Ouyang <flavia.ouyang@mail.mcgill.ca>
2024-03-02 23:47:42 -0500

    Update connector class - Remove redundant code - Fully integrate sqlalchemy

--- molly/connector.py
+++ molly/connector.py
@@ -1,5 +1,5 @@
+from dataclasses import dataclass
 from typing import List, Mapping, Sequence
-from sqlalchemy.engine import URL
 from sqlalchemy import create_engine
 from sqlalchemy import text
 import pandas as pd
@@ -8,60 +8,20 @@ import logging
 logger = logging.getLogger(__name__)
 
 
+@dataclass
 class SQLConnector:
-    def __init__(
-        self,
-        drivername: str,
-        username: str | None = None,
-        password: str | None = None,
-        host: str | None = None,
-        port: int | None = None,
-        database: str | None = None,
-        query: Mapping[str, Sequence[str] | str] = {},
-    ):
-        self.drivername = drivername
-        self.username = username
-        self.password = password
-        self.host = host
-        self.port = port
-        self.database = database
-        self.query = query
+    db_url: str
 
-    @property
-    def url(self):
-        return URL.create(
-            drivername=self.drivername,
-            username=self.username,
-            password=self.password,
-            host=self.host,
-            port=self.port,
-            database=self.database,
-        )
+    def __post_init__(self):
+        self.engine = create_engine(self.db_url)
 
-    @property
-    def engine(self):
-        return create_engine(self.url)
-
-    def select(
-        self,
-        schema: str,
-        table: str,
-        where: List[dict] | None = None,
-        limit: int | None = None,
+    def read_table(
+        self, schema_name: str, table_name: str, parse_dates: dict, **kwargs
     ) -> pd.DataFrame:
-        query = f"SELECT * FROM {schema}.{table}"
-        if where:
-            query += " WHERE"
-            for condition in where:
-                column = condition["column"]
-                value = condition["value"]
-                operator = condition["operator"]
-                query += f" '{column}' {operator} '{value}' AND"
-            query = query[:-4]
-        if limit:
-            query += f" LIMIT {limit}"
-        logger.info(f"Executing query: {query}")
-        with self.engine.connect() as conn:
-            result = conn.execute(text(query))
-            result = pd.DataFrame(result.fetchall())
-        return result
+        return pd.read_sql_table(
+            table_name=table_name,
+            con=self.engine,
+            schema=schema_name,
+            parse_dates=parse_dates,
+            **kwargs,
+        )
