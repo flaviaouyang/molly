c3248e23a4019ef214d7bb680e1ec237796c64bd
Flavia Ouyang <flavia.ouyang@mail.mcgill.ca>
2024-04-04 18:42:34 -0400

    Update parse date method

--- molly/utility/datetime.py
+++ molly/utility/datetime.py
@@ -1,40 +1,54 @@
 import logging
-from datetime import datetime, timedelta, timezone
 from typing import Any, Dict, Tuple, Union
+
 from dateutil import parser
+
+from datetime import datetime, timedelta, timezone
 
 logger = logging.getLogger(__name__)
 
 
-def parse_date(value: Union[str, int, datetime, Tuple[str, int], Dict[str, Any]]) -> datetime:
+def parse_date(
+    value: Union[str, int, datetime, Tuple[str, int], Dict[str, Any]],
+) -> datetime:
     if isinstance(value, datetime):
-        logger.debug(f"Value {value} is already a datetime object. Returning as is.")
+        logger.debug(f"Value {value} is already a datetime object. Returning the same.")
         return value
+
+    if isinstance(value, str):
+        parsed_value = parser.parse(value)
+        logger.debug(f"Parsed {value} to {parsed_value}")
+        return parsed_value
 
     utc_now = datetime.now(tz=timezone.utc)
     if isinstance(value, int):
-        value = ("days", value)
-    elif isinstance(value, str):
-        parsed_value = parser.parse(value)
+        processed_value = ("days", value)
     elif isinstance(value, dict):
-        value = (value["unit"], value["value"])
+        processed_value = (value["unit"], value["value"])
+    elif isinstance(value, tuple):
+        processed_value = value
+    else:
+        raise TypeError(
+            f"Unsupported value type {type(value)}. Supported types are str, int, dict, Tuple[str, int]"
+        )
 
-    if isinstance(value, Tuple):
-        unit, numeric_value = value
-        assert unit in [
-            "days",
-            "seconds",
-            "microseconds",
-            "milliseconds",
-            "minutes",
-            "hours",
-            "weeks",
-        ], (f"Unsupported unit: {unit}. Supported units are days, seconds, microseconds, milliseconds, minutes, hours, "
-            f"weeks.")
-        assert isinstance(
-            numeric_value, int
-        ), f"If value is a tuple, it should be (str, int) but got (str, {type(numeric_value)})"
-        parsed_value = utc_now + timedelta(**{unit: numeric_value})
+    unit, numeric_value = processed_value
+    assert unit in [
+        "days",
+        "seconds",
+        "microseconds",
+        "milliseconds",
+        "minutes",
+        "hours",
+        "weeks",
+    ], (
+        f"Unsupported unit: {unit}. Supported units are days, seconds, microseconds, milliseconds, minutes, hours, "
+        f"weeks."
+    )
+    assert isinstance(
+        numeric_value, int
+    ), f"If value is a tuple, it should be (str, int) but got (str, {type(numeric_value)})"
+    parsed_value = utc_now + timedelta(**{unit: numeric_value})
 
     logger.debug(f"Parsed {value} to {parsed_value}")
     return parsed_value
