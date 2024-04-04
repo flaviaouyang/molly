1027286b1941887752048030313e11b84bbba4a1
Flavia Ouyang <flavia.ouyang@mail.mcgill.ca>
2024-04-03 22:04:08 -0400

    Add staleness to feature factory

--- molly/features/factory.py
+++ molly/features/factory.py
@@ -1,8 +1,11 @@
 from molly.features.completeness import Completeness
+from molly.features.Staleness import Staleness
 
 
 def feature_factory(feature_name: str, **kwargs):
     if feature_name == "completeness":
         return Completeness(**kwargs)
+    elif feature_name == "staleness":
+        return Staleness(**kwargs)
     else:
         raise ValueError(f"Feature {feature_name} is not supported.")
