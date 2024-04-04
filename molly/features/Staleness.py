16012c73a9489ec0ff25dd50f40fd65377dd43e6
Flavia Ouyang <flavia.ouyang@mail.mcgill.ca>
2024-04-04 18:16:41 -0400

    Implement validate

--- molly/features/Staleness.py
+++ molly/features/Staleness.py
@@ -1,6 +1,5 @@
 import logging
 from dataclasses import dataclass
-from typing import Iterable, Union
 
 import pandas as pd
 from sqlalchemy import Select, select, text
@@ -39,11 +38,36 @@ class Staleness(Feature):
         self.__query_info = query
         return query
 
-    def validate(
-        self, retrieved_data: Union[pd.DataFrame, Iterable[pd.DataFrame]]
-    ) -> bool:
-        # TODO: retrieved_data can also be a generator??? maybe not
-        maxmimum_latency = parse_date(["maximum_latency"])
+    def validate(self, retrieved_data: pd.DataFrame) -> bool:
+        is_validate = True
+        self.__validation_result = "Validation output:"
+        maximum_latency = parse_date(self.requirements["maximum_latency"])
+        logger.debug(f"Maximum Latency: {maximum_latency}")
+        availability_column = self.configurations["availability_column"]
+        group_by_columns = self.configurations.get("group_by_columns", [])
+
+        if not group_by_columns:
+            is_validate = retrieved_data[availability_column].max() < maximum_latency
+            self.__validation_result += f"""
+            There is no group by columns. Data was last updated at {retrieved_data[availability_column].max()}.
+            Validation result: {is_validate}
+            """
+            return is_validate
+
+        self.__validation_result += (
+            f"\nFollowing Group by columns used: {group_by_columns}"
+        )
+        for index, df in retrieved_data.groupby(group_by_columns):
+            is_subgroup_validate = df[availability_column].max() < maximum_latency
+            self.__validation_result += (
+                f"\nIndex: {index}. Data was last updated at {df[availability_column].max()}. "
+                f"Validate: {is_subgroup_validate}"
+            )
+            if not is_subgroup_validate:
+                is_validate = False
+        self.__validation_result += f"\nValidation result: {is_validate}"
+        return is_validate
 
     def describe(self) -> str:
-        pass
+        print(self.__query_info)
+        print(self.__validation_result)
