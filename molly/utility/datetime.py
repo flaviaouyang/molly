9fe91c59378502b35a8dbb2d1a85308434148638
Flavia Ouyang <flavia.ouyang@mail.mcgill.ca>
2024-04-03 22:03:34 -0400

    Add handling for dicts

--- molly/utility/datetime.py
+++ molly/utility/datetime.py
@@ -1,11 +1,12 @@
 import logging
 from datetime import datetime, timedelta, timezone
-from typing import Tuple, Union
+from typing import Any, Dict, Tuple, Union
+from dateutil import parser
 
 logger = logging.getLogger(__name__)
 
 
-def parse_date(value: Union[str, int, datetime, Tuple], **kwargs) -> datetime:
+def parse_date(value: Union[str, int, datetime, Tuple[str, int], Dict[str, Any]]) -> datetime:
     if isinstance(value, datetime):
         logger.debug(f"Value {value} is already a datetime object. Returning as is.")
         return value
@@ -14,7 +15,9 @@ def parse_date(value: Union[str, int, datetime, Tuple], **kwargs) -> datetime:
     if isinstance(value, int):
         value = ("days", value)
     elif isinstance(value, str):
-        parsed_value = datetime.strptime(value, **kwargs)
+        parsed_value = parser.parse(value)
+    elif isinstance(value, dict):
+        value = (value["unit"], value["value"])
 
     if isinstance(value, Tuple):
         unit, numeric_value = value
@@ -26,7 +29,8 @@ def parse_date(value: Union[str, int, datetime, Tuple], **kwargs) -> datetime:
             "minutes",
             "hours",
             "weeks",
-        ], f"Unsupported unit: {unit}. Supported units are days, seconds, microseconds, milliseconds, minutes, hours, weeks."
+        ], (f"Unsupported unit: {unit}. Supported units are days, seconds, microseconds, milliseconds, minutes, hours, "
+            f"weeks.")
         assert isinstance(
             numeric_value, int
         ), f"If value is a tuple, it should be (str, int) but got (str, {type(numeric_value)})"
